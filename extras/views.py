4c56c79bdcfb82a86d24bf65c17fcf5e8f5347e5
unknown <jomarkamante@gmail.com>
2023-11-01 14:47:29 +0800

    ll

--- extras/views.py
+++ extras/views.py
@@ -1,17 +1,7 @@
-from django.shortcuts import render, redirect
-from app.models import ListOfCrimes
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def aboutusView(request):
-
-    list_of_crimes = ListOfCrimes.objects.all()
-
-    if request.user.is_authenticated:
-        return render(request, 'app/classification.html', context={'data':list_of_crimes})
-    elif not request.user.is_authenticated and not request.path_info.startswith('/aboutus'):
-        return redirect('aboutus')
-    else:
-        return render(request, "extras/aboutus.html", context={'data':list_of_crimes})
-    
+    return render(request, "extras/aboutus.html")
